6975fa091f886a9e511c568eff4bd5349507dc6d
Serhii Rudyi <srudyjj@gmail.com>
2019-12-12 18:29:32 +0200

    Add Auth

--- src/api/api.js
+++ src/api/api.js
@@ -10,3 +10,38 @@ export default {
   put: (url, props) => instance.put(url, props),
   delete: (url, props) => instance.delete(url, props)
 };
+
+// fetch("/register", {
+// 	method: "POST",
+// 	credentials: 'include',
+// 	body: JSON.stringify({
+//   		email: "olivier@mail.com",
+//   		password: "bestPassw0rd"
+// 	}),
+// 	headers: {
+// 		'Content-Type': 'application/json'
+// 	}
+// }).then(res => res.json()).then(console.log)
+
+// fetch('/orders', {
+//   method: 'POST',
+//   credentials: 'include',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer <token>'
+//   },
+//   body: JSON.stringify(data)
+// })
+//   .then(res => res.json())
+//   .then(console.log);
+
+// axios.defaults.headers.common['Authorization'] =
+//                                 'Bearer ' + localStorage.getItem('jwtToken');
+
+// // Set config defaults when creating the instance
+// const instance = axios.create({
+//   baseURL: 'https://api.example.com'
+// });
+
+// // Alter defaults after instance has been created
+// instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
